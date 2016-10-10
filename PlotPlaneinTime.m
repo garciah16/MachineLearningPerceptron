8bcf31d127bfb042bdc6b5d235fa73688856c9dd
garciah16 <garciah16@up.edu>
2016-10-09 22:53:14 -0400

    gifs!
    the scripts were edited to create animated gifs of the visualization

--- PlotPlaneinTime.m
+++ PlotPlaneinTime.m
@@ -1,15 +1,15 @@
 % implement w(i) = w(i) + N*[c(x)-h(x)]*x(i)
-clear all
+clear
 close all
 load('trainingSet.mat')
 load('cx.mat')
 load('hx.mat')
 %load('firstweights.mat')
-firstWeights = 2*rand(1, 4)-1;
+firstWeights = [0.8235    0.4184   -0.7385    0.4801];
 weights = firstWeights;
 
-N = 0.005;
-numEpoch = 5000;
+N = 0.05;
+numEpoch = 500;
 errorConverge = zeros(numEpoch, 1);
 ep = 1:numEpoch;
 
@@ -20,7 +20,7 @@ hold on
 scatter3(trainingSet(1, 2), trainingSet(1, 3), trainingSet(1, 4), '*')
 lowLim = -0.5;
 upLim = 1.5;
-[x, y] = meshgrid(lowLim:0.1:upLim);
+[x, y] = meshgrid(lowLim:0.5:upLim);
 xlim([lowLim,upLim])
 ylim([lowLim,upLim])
 zlim([lowLim upLim])
@@ -28,7 +28,9 @@ xlabel('x1')
 ylabel('x2')
 zlabel('x3')
 title('Linear Classifier plane convergence')
-view(-10,35)
+view(67,28)
+
+filename = 'PlaneinTime.gif';
 
 for epoch = 1:numEpoch;
     errorCounter = 0;
@@ -56,7 +58,18 @@ for epoch = 1:numEpoch;
     % z = -1/(w3)*(w1*x + w2*y + w0)
     z = (-1)/(weights(4))*(weights(2)*x + weights(3)*y + weights(1));
     h1 = surf(x, y, z, 'FaceAlpha', 0.5, 'LineStyle', '--', 'EdgeAlpha', 0.2);
-    pause(0.05)
+
+    pause(0.1)
+    
+    drawnow;
+    frame = getframe(gcf);
+    im = frame2im(frame);
+    [imind,cm] = rgb2ind(im,256);
+    if epoch == 1
+        imwrite(imind,cm,filename,'gif','Loopcount',inf);
+    else
+        imwrite(imind,cm,filename,'gif','WriteMode','append');
+    end
 
     % check the error and break if there is none
     totErr = errorCounter/100;
@@ -70,3 +83,4 @@ for epoch = 1:numEpoch;
     end
 end
 title(['Linear classifier plane converged in ', num2str(epoch),' epochs'])
+% close(v)
--- plotErrorRate.m
+++ plotErrorRate.m
@@ -1,8 +1,11 @@
 clear
 close all
+
+filename = 'ErrorRateChange.gif';
+
 for nLoop = 1:75
     
-    if(nLoop == 58)
+    if(nLoop == 30)
         db = 1;
     end
     movieMode = 1;
@@ -25,5 +28,17 @@ for nLoop = 1:75
     xlim([1, xlimit])
     ylim([0 max(errorConverge)+0.01])
     
-    pause (0.75)
+    pause (0.0075)
+    
+    if(0)
+        drawnow;
+        frame = getframe(gcf);
+        im = frame2im(frame);
+        [imind,cm] = rgb2ind(im,256);
+        if nLoop == 1
+            imwrite(imind,cm,filename,'gif','Loopcount',inf);
+        else
+            imwrite(imind,cm,filename,'gif','WriteMode','append');
+        end
+    end
 end
